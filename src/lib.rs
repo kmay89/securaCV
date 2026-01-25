8d937863700bb40f1df666e005a8134702a33229
kmay89 <meves.8@gmail.com>
2026-01-25 04:56:13 -0500

    Add V4L2 ingest backend feature

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,8 @@ pub use frame::{
     StubDetector, MAX_BUFFER_FRAMES, MAX_PREROLL_SECS,
 };
 pub use ingest::{rtsp::RtspConfig, RtspSource};
+#[cfg(feature = "ingest-v4l2")]
+pub use ingest::{v4l2::V4l2Config, V4l2Source};
 pub use module_runtime::{CapabilityBoundaryRuntime, ModuleCapability};
 pub use vault::{Vault, VaultConfig};
 
