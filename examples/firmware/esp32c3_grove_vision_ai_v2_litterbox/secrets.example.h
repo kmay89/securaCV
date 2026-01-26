3f29784e75c43e448c4e15ecbf4565799a801720
kmay89 <meves.8@gmail.com>
2026-01-25 19:53:20 -0500

    Add ignored Arduino WiFi secrets template

---
+++ examples/firmware/esp32c3_grove_vision_ai_v2_litterbox/secrets.example.h
@@ -0,0 +1,7 @@
+// Copy this file to secrets.h and fill in your WiFi credentials.
+// secrets.h is gitignored to avoid accidental commits.
+
+#pragma once
+
+static const char* WIFI_SSID = "YOUR_WIFI";
+static const char* WIFI_PASS = "YOUR_PASS";
