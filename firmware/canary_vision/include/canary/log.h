3d2f168a5b66565dc08e30e370be753d5ac84bac
kmay89 <meves.8@gmail.com>
2026-01-29 06:58:25 -0500

    Update log.h
    Signed-off-by: kmay89 <meves.8@gmail.com>

--- firmware/canary_vision/include/canary/log.h
+++ firmware/canary_vision/include/canary/log.h
@@ -7,17 +7,15 @@ static inline uint32_t ms_now() { return millis(); }
 
 // Central place to choose the debug port
 static inline HardwareSerial& dbg_serial() {
-  // ESP32-C3 often prefers Serial, but some configs only expose Serial0/Serial1.
-  // Serial0 is the USB CDC/UART on many ESP32 cores.
-  #if defined(ARDUINO_ARCH_ESP32)
-    #if defined(Serial0)
-      return Serial0;
-    #else
-      return Serial;
-    #endif
+#if defined(ARDUINO_ARCH_ESP32)
+  #if defined(Serial0)
+    return Serial0;
   #else
     return Serial;
   #endif
+#else
+  return Serial;
+#endif
 }
 
 static inline void log_header(const char* tag) {
@@ -35,3 +33,9 @@ static inline void log_kv(const char* tag, const char* k, const char* v) {
 }
 
 } // namespace canary
+
+// ---- Backwards-compatible global wrappers ----
+// (So existing code can keep calling log_line(), ms_now(), etc.)
+static inline uint32_t ms_now() { return canary::ms_now(); }
+static inline void log_line(const char* tag, const char* msg) { canary::log_line(tag, msg); }
+static inline void log_kv(const char* tag, const char* k, const char* v) { canary::log_kv(tag, k, v); }
