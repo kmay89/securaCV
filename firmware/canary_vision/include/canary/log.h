1b747d11259db24be9b62c7fdf67344b4aa765dc
kmay89 <meves.8@gmail.com>
2026-01-29 07:27:11 -0500

    Update log.h
    Signed-off-by: kmay89 <meves.8@gmail.com>

--- firmware/canary_vision/include/canary/log.h
+++ firmware/canary_vision/include/canary/log.h
@@ -1,27 +1,25 @@
-// include/canary/log.h
 #pragma once
 #include <Arduino.h>
 
 namespace canary {
 
+// ESP32-C3 does NOT reliably provide `Serial`
 static inline HardwareSerial& dbg_serial() {
-#if defined(ARDUINO_ARCH_ESP32) || defined(ESP_PLATFORM)
-  return Serial;
+#if defined(ARDUINO_ARCH_ESP32) && defined(CONFIG_IDF_TARGET_ESP32C3)
+  return Serial0;
 #else
-  // Fallback: still compiles in weird CI targets
-  return Serial1;
+  return Serial;
 #endif
 }
 
 static inline uint32_t ms_now() { return millis(); }
 
 static inline void log_header(const char* tag) {
-  dbg_serial().printf("\n[%s]\n", tag);
+  dbg_serial().printf("\n[%s]\n", tag ? tag : "");
 }
 
 static inline void log_line(const char* tag, const char* msg) {
-  dbg_serial().printf("[%s] %s\n", tag, msg);
+  dbg_serial().printf("[%s] %s\n", tag ? tag : "", msg ? msg : "");
 }
 
 } // namespace canary
-
