d186e2f0bdff0cf639b5a583fc1577abd47d8c8b
kmay89 <meves.8@gmail.com>
2026-01-29 06:48:27 -0500

    Refactor VoxelTracker member variable initialization
    Signed-off-by: kmay89 <meves.8@gmail.com>

--- firmware/canary_vision/include/canary/state/voxel_tracker.h
+++ firmware/canary_vision/include/canary/state/voxel_tracker.h
@@ -5,16 +5,23 @@ namespace canary::state {
 
 class VoxelTracker {
 public:
+  VoxelTracker()
+    : cur_(Voxel{255, 255, 0, 0}),
+      stable_(Voxel{255, 255, 0, 0}),
+      stable_frames_(0),
+      stable_enter_ms_(0) {}
+
   void reset();
   void update(const Voxel& v, uint32_t now_ms);
+
   Voxel stable() const { return stable_; }
   uint32_t stable_enter_ms() const { return stable_enter_ms_; }
 
 private:
-  Voxel cur_{255, 255, 0, 0};
-  Voxel stable_{255, 255, 0, 0};
-  uint8_t stable_frames_=0;
-  uint32_t stable_enter_ms_=0;
+  Voxel cur_;
+  Voxel stable_;
+  uint8_t stable_frames_;
+  uint32_t stable_enter_ms_;
 };
 
-} // namespace
+} // namespace canary::state
